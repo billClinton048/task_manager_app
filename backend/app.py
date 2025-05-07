4f348e5ebc9c3ad057b4d4cc08019f7c8123679e
billClinton048 <clintonkaranja46@gmail.com>
2025-05-07 23:04:49 +0330

    added the cors

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
-
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
@@ -9,6 +9,7 @@ def create_app():
 	app = Flask(__name__)
 	app.config.from_object(Config)
 	db.init_app(app)
+	CORS(app)
 
 	with app.app_context():
 		from models import Task
